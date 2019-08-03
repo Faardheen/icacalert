9f60d533c8a2dc9a01b180d07b366f9cde9e20c4
williamjosh <williamjosh121@gmail.com>
2019-08-03 20:39:35 +0400

    fixed grid issues

--- app/routes/Home.js
+++ app/routes/Home.js
@@ -1,49 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const AppLayout = styled.div`
-	display: grid;
-	height: 105vh;
-`;
-
-const Sidebar = styled.div`
-	background-color: #033875;
-	padding-right: 0px !important;
-`;
-
-const SidebarList = styled.ul`
-	list-style: none;
-	padding: 0px;
-	margin: 0px;
-	display: inline-block;
-	width: 100%;
-	font-size: 18px;
-`;
-
-const SidebarItem = styled.li`
-	text-align: center;
-	color: white;
-	padding: 20px;
-`;
+import SideBar from '../containers/Sidebar';
+import MainMenu from '../containers/MainMenu';
+import ContentPage from '../containers/ContentPage';
+import AppLayout from '../components/AppLayout';
 
 export default () => (
-	<AppLayout className='ui grid'>
-		<div className='sixteen column row'>
-			<Sidebar className='one column'>
-				<SidebarList>
-					<SidebarItem>
-						<i className='home icon' />
-					</SidebarItem>
-					<SidebarItem>
-						<i className='flag icon' />
-					</SidebarItem>
-					<SidebarItem>
-						<i className='eye icon' />
-					</SidebarItem>
-				</SidebarList>
-			</Sidebar>
-			<div className='orange three wide column'>pwdkp</div>
-			<div className='teal twelve wide column'>pwdkp</div>
+	<AppLayout>
+		<div className='ui grid'>
+			<SideBar />
+			<div className='black fifteen wide column'>
+				<div className='ui grid'>
+					<MainMenu />
+					<ContentPage />
+				</div>
+			</div>
 		</div>
 	</AppLayout>
 );
